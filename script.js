bbb4d9ca49456bd2b7fd3d878b48e3160203d869
Evandro Vale <46692901+eClosed@users.noreply.github.com>
2021-10-03 20:45:28 -0300

    Adicionado certificard #imersaodev #alura

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+// Utilizaremos o Figma para transformar o design em código
+// Estruturamos o nosso portfólio com HTML, aprendendo todas as tags necessárias pra isso
+// Estilizamos o nosso portfólio com CSS, conhecendo os seletores, propriedades e valores necessários para isso
+// Criamos o tema claro e escuro para o nosso portfólio
+// Fazemos a lógica em Javascript para alterar entre os temas
